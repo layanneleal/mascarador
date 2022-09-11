3d0370131cfb0dd21a276b788fe285e5b65c62b9
Layanne Bezerra Leal <layanneliz@gmail.com>
2022-09-11 12:37:12 -0300

    Uso de substring

--- mascarador.js
+++ mascarador.js
@@ -1,16 +1,23 @@
+/*
+9815 0000 0000 1234
+**** **** **** 1234
+Mascarar os 12 primeiros números 
+Exibir os 4 últimos números
+
 const input = document.getElementById('ent')
 
 input.addEventListener('keypress', () => {
   let inputlength = input.value.length
   if (inputlength === 4 || inputlength ===9 || inputlength === 14) {
     input.value += ' '
-
   }
 })
-
-  /*
   if (input.length === 4) {
     input.value += '.'
+} 
+*/
+let cartao = prompt('Informe o número do seu cartão')
+const numerosMask = cartao.length - 4
 
-  } */
+alert('*'.repeat(numerosMask) + cartao.substring(numerosMask))
 
